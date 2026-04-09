d3924b11faa7722d618209c72fa845f6e7d87efc
CalvinTrainee <129370257+CalvinTrainee@users.noreply.github.com>
2026-04-09 13:00:51 +0800

    calvin-nexus-system

---
+++ calvin-nexus-system/calvin-nexus-system-service/src/main/java/com/calvinnexus/system/mapper/DictionaryMapper.java
@@ -0,0 +1,15 @@
+package com.calvinnexus.system.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.calvinnexus.system.model.po.Dictionary;
+
+/**
+ * <p>
+ * 数据字典 Mapper 接口
+ * </p>
+ *
+ 
+ */
+public interface DictionaryMapper extends BaseMapper<Dictionary> {
+
+}
