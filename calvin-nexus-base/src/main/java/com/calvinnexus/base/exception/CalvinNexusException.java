2e240a0767df60bc18156ba43325be177c763132
CalvinTrainee <129370257+CalvinTrainee@users.noreply.github.com>
2026-04-09 12:57:53 +0800

    calvin-nexus-base

---
+++ calvin-nexus-base/src/main/java/com/calvinnexus/base/exception/CalvinNexusException.java
@@ -0,0 +1,36 @@
+package com.calvinnexus.base.exception;
+
+/**
+ * @version 1.0
+ * @description 本项目自定义异常类型
+ */
+public class CalvinNexusException extends RuntimeException {
+
+    private String errMessage;
+
+    public CalvinNexusException() {
+    }
+
+    public CalvinNexusException(String message) {
+        super(message);
+        this.errMessage = message;
+
+    }
+
+    public String getErrMessage() {
+        return errMessage;
+    }
+
+    public void setErrMessage(String errMessage) {
+        this.errMessage = errMessage;
+    }
+
+    public static void cast(String message) {
+        throw new CalvinNexusException(message);
+    }
+
+    public static void cast(CommonError error) {
+        throw new CalvinNexusException(error.getErrMessage());
+    }
+
+}
---
+++ calvin-nexus-base/src/main/java/com/calvinnexus/base/exception/ValidationGroups.java
@@ -0,0 +1,22 @@
+package com.calvinnexus.base.exception;
+
+/**
+ * @version 1.0
+ * @description 用于分级校验，定义一些常用的组
+ */
+public class ValidationGroups {
+    public interface Inster {
+    }
+
+    ;
+
+    public interface Update {
+    }
+
+    ;
+
+    public interface Delete {
+    }
+
+    ;
+}
