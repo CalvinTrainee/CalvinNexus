9208a62310e9106da46ada630c085cf9f36da917
CalvinTrainee <129370257+CalvinTrainee@users.noreply.github.com>
2026-04-09 12:59:33 +0800

    calvin-nexus-gateway

---
+++ calvin-nexus-gateway/src/test/java/com/calvinnexus/CalvinNexusGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calvinnexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalvinNexusGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
